53ac9e675587e6b00cdd40e0b35cf97b68ef6b09
Jack Lowrie <jacklowrie@me.com>
2025-11-15 14:50:08 -0500

    crude test cases

--- public/main.py
+++ public/main.py
@@ -1,20 +1,29 @@
 # main.py
 from pyscript import PyWorker, document, window
 
+# ---- Initialize Ace Editor from Python ----
+# Create the editor
+editor = window.ace.edit(document.getElementById("solution"))
+editor.setTheme("ace/theme/tomorrow")
+editor.session.setMode("ace/mode/python")
+editor.setOptions({
+    "fontSize": "14px",
+    "showPrintMargin": False,
+    "tabSize": 4,
+    "useSoftTabs": True,
+    "wrap": True
+})
+
+editor.setValue("def multiply(a, b):\n    # your code here ")
+editor.resize()
+
 worker = PyWorker("worker.py", type="pyodide")
-
-worker.sync.greetings = lambda: print("Pyodide bootstrapped")
-
-print("before ready")
 await worker.ready
-print("after ready")
 
 async def run(event):
-    code = window.editor.getValue()
+    code = editor.getValue()
     result = await worker.sync.evaluate(code)
-    print(result)
-    document.body.append(result)
+    document.getElementById("output").innerText = f"Results:\n {result}"
 
 document.getElementById("run").addEventListener("click", run)
-# worker.terminate()
 
--- public/worker.py
+++ public/worker.py
@@ -1,22 +1,55 @@
 # worker.py
-import sys
 from pyscript import sync
 
-# Use any methods from main.py on the main thread.
-sync.greetings()
-
-
 def evaluate(code):
+    code = code.replace("\t", "    ")
+    print(code)
+    namespace = {}
     try:
-        namespace = {}
         exec(code, namespace)
-        return namespace["multiply"](6, 7)
     except Exception as e:
-        return f"error: {e}"
+        return f"💥 Code failed to compile:\n{e}"
+
+    if "multiply" not in namespace:
+        return "💥 multiply function not defined"
+
+    tests = [
+        (1,1),
+        (1,2),
+        (2,1),
+        (2,3),
+        (0,3),
+        (3,0),
+        (-2,3),
+        (2,-3),
+    ]
+    results = []
+    for test in tests:
+        try:
+            exec(code, namespace)
+            if "multiply" not in namespace:
+                results.append("error: 'multiply' function not defined")
+                continue
+            results.append(namespace["multiply"](test[0], test[1]))
+        except Exception as e:
+            results.append( f"error: {e}")
+    
+    formatted = ""
+    for test, result in zip(tests, results):
+        if str(result).startswith("error:"):
+            formatted += f"💥 multiply{test} raised an exception: {result[7:]}\n"
+        elif result == test[0] * test[1]:
+            formatted += f"✅ multiply{test} = {result} (correct)\n"
+        else:
+            formatted += f"❌ multiply{test} = {result} (incorrect, expected {test[0] * test[1]})\n"
+
+    return formatted
+
+
+
 
 
 
 # Expose any methods meant to be used from main.
 sync.evaluate = evaluate
-sync.heavy_computation = lambda: 6 * 7
 
