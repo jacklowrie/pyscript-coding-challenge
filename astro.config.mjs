97642e164832495305c7d937c305aec1a7e09e95
Jack Lowrie <jacklowrie@me.com>
2025-12-17 14:39:56 -0500

    update config for ghpages deploys

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jacklowrie.github.io',
+  base: '/pyscript-coding-challenge',
   integrations: [icon()]
 });
